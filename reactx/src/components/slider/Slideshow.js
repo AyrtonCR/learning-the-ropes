df8ae63f831e72b45fa3fbae11ac59c275ac2734
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-04-19 11:56:49 +1200

    progress on slideshow image scaling but it is currently spiking at the breakpoints. slider buttons need positioning and slider design with padding needs to be implemented

--- reactx/src/components/slider/Slideshow.js
+++ reactx/src/components/slider/Slideshow.js
@@ -18,31 +18,35 @@ export default function Slideshow() {
   const prevSlide = () => {};
 
   return (
-    <div className="slide-show-container">
-      {dataSlider.map((obj, index) => {
-        return (
-          <div
-            key={obj.id}
-            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-          >
-            <img
-              className="slide-show-image"
-              alt="slider-pic"
-              src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpeg`}
-            />
-            <p>{obj.subTitle}</p>
-            <div className="container-dots">
-              {Array.from({ length: 3 }).map((item, index) => (
-                <div
-                  onClick={() => moveDot(index + 1)}
-                  className={slideIndex === index + 1 ? "dot active" : "dot"}
-                  key={index}
-                ></div>
-              ))}
+    <div className="ssc">
+      <div className="slide-show-container">
+        {dataSlider.map((obj, index) => {
+          return (
+            <div
+              key={obj.id}
+              className={
+                slideIndex === index + 1 ? "slide active-anim" : "slide"
+              }
+            >
+              <img
+                className="slide-show-image"
+                alt="slider-pic"
+                src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpeg`}
+              />
+              <p>{obj.subTitle}</p>
+              <div className="container-dots">
+                {Array.from({ length: 3 }).map((item, index) => (
+                  <div
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    key={index}
+                  ></div>
+                ))}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <BtnSlider moveSlide={nextSlide} direction={"next"} />
       <BtnSlider moveSlide={prevSlide} direction={"prev"} />
     </div>
